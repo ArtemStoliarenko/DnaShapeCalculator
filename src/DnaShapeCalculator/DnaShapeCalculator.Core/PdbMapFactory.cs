a62cfaebfbb63850a735b08f0c5f3eedde55fe38
Artem Stoliarenko <roguenalan@gmail.com>
2019-07-25 17:46:25 +0300

    started working on factory

---
+++ src/DnaShapeCalculator/DnaShapeCalculator.Core/PdbMapFactory.cs
@@ -0,0 +1,20 @@
+﻿using DnaShapeCalculator.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnaShapeCalculator.Core
+{
+    public static class PdbMapFactory
+    {
+        public static PdbMap CreatePdbMap(string[] records)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static PdbMapRecord CreatePdbMapRecord(string record)
+        {
+            var data = s
+        }
+    }
+}
